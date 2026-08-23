2959cb205a66257535bf74b0e8917c59b421dd30
Jesse_Carter <jessetcarter@hotmail.com>
2026-08-23 00:20:49 -0600

    adjusted constructor and created load_map_data

--- graph.py
+++ graph.py
@@ -1,42 +1,98 @@
 import csv
-
-"""
-Representation of map.csv:
-
-
-"""
+import collections
+import math
 
 class Graph:
+  """
+  City map containing road edges and graph-node coordinates
+  """
   def __init__(self):
-    self.adjacency_list = {}
+    self.adjacency_list = collections.defaultdict(list) # {}
+    self.node_coordinates: dict = {}
 
-  def add_vertex(self, vertex):
-    if vertex not in self.adjacency_list:
-      self.adjacency_list[vertex] = []
+  # def add_vertex(self, vertex):
+  #   if vertex not in self.adjacency_list:
+  #     self.adjacency_list[vertex] = []
 
-  def add_edge(self, start_node, end_node, weight):
-    self.add_vertex(start_node)
-    self.add_vertex(end_node)
-    self.adjacency_list[start_node].append((end_node, weight))
+  # def add_edge(self, start_node, end_node, weight):
+  #   self.add_vertex(start_node)
+  #   self.add_vertex(end_node)
+  #   self.adjacency_list[start_node].append((end_node, weight))
 
-  def load_from_file(self, filename):
-    print(f"Loading map from {filename}...")
-    try:
-      with open(filename, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-          if len(row) == 3:
-            start, end, weight = row
-            self.add_edge(str(start.strip()), str(end.strip()), int(weight.strip()))
-      print("Map loaded successfully.")
-    except FileNotFoundError:
-      print(f"Error: file '{filename}' not found.")
-    except Exception as e:
-      print(f"An error occurred: {e}")
+  def load_map_data(self, filename):
+    with open(filename, "r") as file:
+      for line in file:
+        if line.startswith("#") or not line.strip():
+          continue
+
+        data = line.strip().split(",")
+
+        (
+          start_id,
+          start_x,
+          start_y,
+          end_id,
+          end_x,
+          end_y,
+          weight
+        ) = data
+
+        self.node_coordinates[start_id] = (
+          float(start_x),
+          float(start_y)
+        )
+
+        self.node_coordinates[end_id] = (
+          float(end_x),
+          float(end_y)
+        )
+
+        self.adjacency_list[start_id].append(
+          (end_id, float(weight))
+        )
+
+        self.adjacency_list[end_id].append(
+          (start_id, float(weight))
+        )
+
+  # Snap coordinates to Graph Vertices
+  def find_nearest_vertex(self, point: tuple) -> str | None:
+    x_p, y_p = point
+    shortest_distance = math.inf
+    vertex = None
+
+    for node, coordinates in self.node_coordinates.items():
+      x_n, y_n = coordinates
+      distance = (x_n - x_p)**2 + (y_n - y_p)**2
+      if distance < shortest_distance:
+        shortest_distance = distance
+        vertex = node
+
+    if vertex == None:
+      raise ValueError("Graph vertices were not loaded.")
+
+    return vertex
+
+  # def load_from_file(self, filename):
+  #   print(f"Loading map from {filename}...")
+  #   try:
+  #     with open(filename, 'r') as f:
+  #       reader = csv.reader(f)
+  #       for row in reader:
+  #         if len(row) == 3:
+  #           start, end, weight = row
+  #           self.add_edge(str(start.strip()), str(end.strip()), int(weight.strip()))
+  #     print("Map loaded successfully.")
+  #   except FileNotFoundError:
+  #     print(f"Error: file '{filename}' not found.")
+  #   except Exception as e:
+  #     print(f"An error occurred: {e}")
 
   def __str__(self):
     return f"""
 Graph Adjacency List:
 
 {"\n".join([f"{key} -> {val}" for key, val in self.adjacency_list.items()])}
+
+{"\n".join([f"{key} -> {val}" for key, val in self.node_coordinates.items()])}
 """
